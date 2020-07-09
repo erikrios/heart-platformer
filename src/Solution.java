61f587f3863e08776844862edaa553d292c71bd6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-09 22:36:54 +0700

    Add the solution code

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 1;
+
+        do {
+            String input = scanner.nextLine();
+            System.out.println(number + " " + input);
+            number++;
+        } while (scanner.hasNext());
     }
 }
