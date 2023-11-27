0c34cc3c68925aa46db439b345c2b871685814bd
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-27 11:32:39 +0700

    Adjust the UI theming

--- world.gd
+++ world.gd
@@ -5,12 +5,12 @@ extends Node2D
 @export var next_level: PackedScene
 
 func _ready() -> void:
-	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed() -> void:
 	level_completed.show()
 	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
 	if not next_level is PackedScene:
 		return
 	await LevelTransition.fade_to_black()
