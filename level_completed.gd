aece855bcdb78d421d798c02f0ce7edffe5cc47d
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-27 21:05:00 +0700

    Adding victory screen

--- level_completed.gd
+++ level_completed.gd
@@ -4,6 +4,7 @@ signal retry()
 signal next_level()
 
 @onready var retry_button: Button = %RetryButton
+@onready var next_level_button: Button = %NextLevelButton
 
 func _on_retry_button_pressed() -> void:
 	retry.emit()
--- world.gd
+++ world.gd
@@ -12,10 +12,13 @@ var start_level_msec := 0.0
 @onready var level_time_label: Label = %LevelTimeLabel
 
 func _ready() -> void:
+	if not next_level is PackedScene:
+		level_completed.next_level_button.text = "Victory Screen"
+		next_level = load("res://victory_screen.tscn")
 	Events.level_completed.connect(show_level_completed)
 	get_tree().paused = true
 	start_in.visible = true
-	await LevelTransition.fade_from_black()
+	LevelTransition.fade_from_black()
 	animation_player.play("countdown")
 	await animation_player.animation_finished
 	get_tree().paused = false
